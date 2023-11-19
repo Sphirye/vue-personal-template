47fded1a4ec271c751bfb3831b73df0b531dddc1
sphirye <ronielcontreras70@gmail.com>
2023-11-19 12:42:04 -0500

    Add subtitle, exclusion and divider fields in DrawerItem.ts model

--- src/model/vue/DrawerItem.ts
+++ src/model/vue/DrawerItem.ts
@@ -3,8 +3,11 @@ import { AuthorityName } from "@/model/Authority"
 export default class DrawerItem {
     title?: string = undefined
     icon?: string = undefined
+    subtitle?: string = undefined
     subheader?: string = undefined
     to?: string = undefined             // Route target
-    global?: boolean = false            // The item will be showed for everyone
+    global?: boolean = true            // The item will be showed for everyone by default
     allowed?: AuthorityName[] = []      // The item will be showed to these roles when global is false
+    exclusions?: AuthorityName[] = []   // The item will hide for the specified roles
+    divider?: boolean = false
 }
